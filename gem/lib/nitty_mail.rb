84c7c5b59dd493bb4c0d0bdb3444f845bd6418f6
parasquid <git@parasquid.com>
2025-09-04 16:00:56 +0800

    fix requires

--- gem/lib/nitty_mail.rb
+++ gem/lib/nitty_mail.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative "NittyMail/version"
+require_relative "nitty_mail/version"
 
 module NittyMail
   class Error < StandardError; end
   # Your code goes here...
 end
 
-require_relative "NittyMail/settings"
-require_relative "NittyMail/errors"
-require_relative "NittyMail/mailbox"
+require_relative "nitty_mail/settings"
+require_relative "nitty_mail/errors"
+require_relative "nitty_mail/mailbox"
